c2fe0f18c0400da3b742591c5e9dc1e93e610365
saifuldhaka <abssaiful@gmail.com>
2022-04-11 22:53:21 +0800

    first commit

---
+++ app/Http/Controllers/CompaniesController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Companies;
+use App\Http\Requests\StoreCompaniesRequest;
+use App\Http\Requests\UpdateCompaniesRequest;
+
+use App\Models\Companies;
+
+class CompaniesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return view('companies.index', [
+          'users' => Companies::paginate(15)
+      ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCompaniesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCompaniesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Companies  $companies
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Companies $companies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Companies  $companies
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Companies $companies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCompaniesRequest  $request
+     * @param  \App\Models\Companies  $companies
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCompaniesRequest $request, Companies $companies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Companies  $companies
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Companies $companies)
+    {
+        //
+    }
+}
